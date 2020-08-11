55c5a4aa577bee04299fff0671b1411fb866c819
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-08-11 16:22:01 +0300

    proptypes were added

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 //styles
 import './ContactCard.styles.css';
 
-const ContactCard = (props) => {
+const ContactCard = ({ avatar, name, email, age }) => {
   const [showAge, setShowAge] = useState(false);
 
   return (
     <div className="contact-card">
-      <img src={props.avatar} alt="profile" />
+      <img src={avatar} alt="profile" />
       <div className="user-details">
-        <p>Name: {props.name}</p>
-        <p>Email: {props.email}</p>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
         <button onClick={() => setShowAge(!showAge)}>Toggle Age</button>
-        {showAge && <p>Age: {props.age}</p>}
+        {showAge && <p>Age: {age}</p>}
       </div>
     </div>
   );
 };
+
+ContactCard.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  age: PropTypes.number,
+};
+
+ContactCard.defaultProps = {
+  avatar: '#',
+  name: 'Liliia',
+  email: '#',
+  age: '',
+};
+
 export default ContactCard;
